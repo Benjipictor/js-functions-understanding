e9ba5447d7c721e17ca311434b655b46bd10e997
benji <benjipictor@gmail.com>
2022-11-16 12:30:31 +0000

    Q11

---
+++ answers-to-questions.js
@@ -0,0 +1,136 @@
+// // // Q1
+// // function myFunction(num1, num2) {
+// //     return num1+num2
+// //   }
+  
+// //   const result = myFunction(5,5)
+
+// // //   this funstion will add together 5 and 5 because the its returning num1 + num2
+
+// // // Q2
+// // // function myFunction(num1, num2) {
+// // //     num1+num2
+// // //   }
+  
+// // //   const result = myFunction(5,5)
+
+// // //   it will return undefiend because it dosent specify what to do with the return key word
+
+// // // Q3
+
+// // function myFunction(num) {
+// //     return num-1
+// //   }
+  
+// //   let num = 10
+// //   num = myFunction(num)
+// //   num = myFunction(num)
+
+// // //   it should return 8 becaus it repeats the funstion twice.
+
+// // // Q4
+// // function myFunction(num) {
+// //     return num-1
+// //   }
+  
+// //  let num = 10
+// //   let add = 3
+// //   add = myFunction(add)
+// //   add = myFunction(add)
+
+// // //   it will print out 1 if you dont get a syntax error
+
+// // Q5
+
+// function myFunction(num, num1) {
+//     console.log(num1)
+//   }
+  
+//   let num = 10
+//   let num1 = 2
+  
+//   myFunction(num)
+
+// //   will say undefiend because no value is assighned to num inside the funstion
+
+// Q6
+
+// function myFunction(num, num1) {
+//     console.log(num1)
+//   }
+  
+//   let num = 10
+//   let num1 = 2
+  
+//   myFunction(num1, num)
+
+//   we only get 10 in the console because num1 isent mentioned in the operation.
+
+// Q7
+
+// let counter = 1
+
+// function myFunction() {
+//   counter++
+//   return counter
+// }
+
+// myFunction()
+// const num = myFunction()
+
+// console.log(counter)
+// the counter inscreases by 2 because counter ++
+
+// Q8
+// function myFunction(num1, num2) {
+//     return num1 + num2
+//   }
+  
+//   const num1 = 10
+//   const num2 = 1
+//   const num3 = 4
+//   const num4 = 11
+//   const num5 = 12
+//   const result = myFunction(11, 2)
+
+//   console.log(result)
+//   it returns 14 bwecause it draws its values from the const available following thr structure of the funstion.
+
+// Q9
+// function myFunction(num1, num2) {
+//     console.log(num3)
+//   }
+  
+//   const num1 = 10
+//   const num2 = 1
+//   const num3 = 20
+  
+//   myFunction(num3, num1)
+// console.log(myFunction)
+  //   because its only printing the vaue assighned to num3 it dosent do anything else.
+
+// Q10
+function myFunction(num1, num2, num3) {
+    console.log(num3)
+  }
+  
+  const num1 = 10
+  const num2 = 1
+  const num3 = 20
+  
+  myFunction(num3, num1, 100)
+console.log(myFunction)
+  //   because theires only a fixed value 
+
+//   Q11
+  function myFunction(num1, num2, num3) {
+    return num1 + num2 + num3
+  }
+  
+  const num1 = 10
+  const num2 = 1
+  const num3 = 20
+  
+  const result = myFunction(1, 1, 1)
+
+  
